144be43d0c016c735820200d896e3b9adf3d0de9
Sudhanshu Pathak <connect.sudhanshupathak@gmail.com>
2026-04-29 18:16:50 +0530

    Initial commit

---
+++ src/main/java/Inheritance/inh4.java
@@ -0,0 +1,26 @@
+package Inheritance;
+
+
+class Calculator{
+    int add(int a,int b){
+        return a+b;
+    }
+    int add(int a,int b,int c){
+        return a+b+c;
+    }
+    double add(double a,double b){
+        return a+b;
+    }
+    String add(String a,String b){
+        return a+b;
+    }
+}
+public class inh4 {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println(c.add(1,2));
+        System.out.println(c.add(1,2,3));
+        System.out.println(c.add(2.0,3.0));
+        System.out.println(c.add("Sudhanshu ","Pathak"));
+    }
+}
